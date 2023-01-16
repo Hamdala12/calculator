af983efa37b9129d4bbe1679de34e8405e290479
Ahmad B <ahmadbello2011@gmail.com>
2023-01-16 10:38:36 -0800

    calculator

---
+++ style.js
@@ -0,0 +1,104 @@
+ //clear
+ document.getElementById("clear").addEventListener("click", ()=>{
+    document.getElementById("display").value = "";
+})
+
+//backspace 
+
+ function Ac(){
+    let display = document.getElementById("display")
+    display.value = display.value.slice(0, -1)
+ }
+    
+     const one = () =>{
+        if(document.getElementById('display').value == ""){
+            document.getElementById('display').value = "1";
+        }
+        else{
+            document.getElementById('display').value = document.getElementById('display').value + "1"
+        }
+     }
+
+     const two = () =>{
+        if(document.getElementById('display').value == ""){
+            document.getElementById('display').value = "2";
+        }
+        else{
+            document.getElementById('display').value = document.getElementById('display').value + "2"
+        }
+     }
+
+     const three = () =>{
+        if(document.getElementById ('display').value == ""){
+            document.getElementById('display').value = "3";
+        }
+        else{
+            document.getElementById('display').value = document.getElementById('display').value + "3"
+        }
+     }
+
+     const four = () =>{
+        if(document.getElementById('display').value == ""){
+            document.getElementById('display').value = "4";
+        }
+        else{
+            document.getElementById('display').value = document.getElementById('display').value + "4"
+        }
+     }
+
+     const five = () =>{
+        if(document.getElementById('display').value == ""){
+            document.getElementById('display').value = "5";
+        }
+        else{
+            document.querySelector('#display').value = document.getElementById('display').value + "5"
+        }
+     }
+     
+     const six = () =>{
+        if(document.getElementById('display').value == ""){
+            document.getElementById('display').value = "6";
+        }
+        else{
+            document.querySelector('#display').value = document.getElementById('display').value + "6"
+        }
+     }
+     
+     const seven = () =>{
+        if(document.getElementById('display').value == ""){
+            document.getElementById('display').value = "7";
+        }
+        else{
+            document.getElementById('display').value = document.getElementById('display').value + "7"
+        }
+     }
+     
+     const eight = () =>{
+        if(document.getElementById('display').value == ""){
+            document.getElementById('display').value = "8";
+        }
+        else{
+            document.getElementById('display').value = document.getElementById('display').value + "8"
+        }
+     } 
+     
+    const nine = () =>{
+        if(document.getElementById('display').value == ""){
+            document.getElementById('display').value = "9";
+        }
+        else{
+            document.getElementById('display').value = document.getElementById('display').value + "9"
+        }
+     }
+
+     const zero = () =>{
+        if(document.getElementById('display').value == ""){
+            document.getElementById('display').value = "0";
+        }
+        else{
+            document.getElementById('display').value = document.getElementById('display').value + "0"
+        }
+     }
+
+
+    
